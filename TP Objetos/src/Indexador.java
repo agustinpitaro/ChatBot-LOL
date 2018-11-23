20e17826a2ff0704ad7ccbb2f12c905f1a06930d
Agustin <Agustin@192.168.0.3>
2018-11-23 11:15:50 -0300

    Eclipse working

--- TP Objetos/src/Indexador.java
+++ TP Objetos/src/Indexador.java
@@ -43,31 +43,6 @@ public class Indexador {
 		this.config = new IndexWriterConfig(sA);
 		
 	}
-		   
-	/*public static void call_me() throws Exception {
-		 String apiKey = "ec545d402ebd648449b6cf282cf288fb";
-	     String url = "http://api.champion.gg/v2/champions?elo=GOLD&limit=200&api_key=" + apiKey;
-	     URL obj = new URL(url);
-	     HttpURLConnection con = (HttpURLConnection) obj.openConnection();
-	     // optional default is GET
-	     con.setRequestMethod("GET");
-	     //add request header
-	     con.setRequestProperty("User-Agent", "Mozilla/5.0");
-	     int responseCode = con.getResponseCode();
-	     System.out.println("\nSending 'GET' request to URL : " + url);
-	     System.out.println("Response Code : " + responseCode);
-	     BufferedReader in = new BufferedReader(
-	             new InputStreamReader(con.getInputStream()));
-	     String inputLine;
-	     StringBuffer response = new StringBuffer();
-	     while ((inputLine = in.readLine()) != null) {
-	     	response.append(inputLine);
-	     }
-	     in.close();
-	     //print in String
-	     System.out.println(response.toString());
-	     }*/
-	   
 	public static void createIndex() throws CorruptIndexException, LockObtainFailedException, IOException {
 		IndexWriter indexWriter = new IndexWriter(directory, config);
 		File dir = new File("Directorio donde esta la info");
@@ -121,9 +96,6 @@ public class Indexador {
 	     document.add(new TextField("Winrate", "0,10", Field.Store.YES));
 	     writer.addDocument(document);
 	     writer.close();
-	     
-	     
-	     
 	     
 	     //Now let's try to search for Hello
 	     IndexReader reader = DirectoryReader.open(directory);
