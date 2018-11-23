65c0d58a8cc6c2349e8e0f7b18ab76789355a1e7
Agustin <Agustin@192.168.0.3>
2018-11-23 15:32:17 -0300

    Por fin anduvo :D

--- TP Objetos/src/Utilities/Test.java
+++ TP Objetos/src/Utilities/Test.java
@@ -3,12 +3,14 @@ package Utilities;
 import Query.Client;
 
 public class Test {
-	
+
 	public static void main(String[] args) {
 		try {
 			Client.search("");
 		} catch (Exception e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-    }
+	}
+
 }
